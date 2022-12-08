79a0ffc476c482e64cdcdd4c67c282192ab2f37b
Wenjing Li <wjingli@umich.edu>
2022-12-08 12:02:39 -0500

    remove 3 from gender

--- R/Code.R
+++ R/Code.R
@@ -1,5 +1,10 @@
 full_model = read.csv("~/Downloads/FINAL/diabetic_data.csv")
+##remove missing data in gender
+full_model = full_model[full_model$gender != "Unknown/Invalid", ]
+
 cl_model = subset(full_model,select = -c(encounter_id,patient_nbr,weight,payer_code, medical_specialty,examide,citoglipton))
+names(cl_model)
+diag = rbind(cl_model$diag_1,cl_model$diag_2,cl_model$diag_3)
 
 a = vector()
 b = list()
@@ -13,3 +18,12 @@ print(a)
 print(b)
 
 length(which(full_model[,4] == "Unknown/Invalid"))
+
+sort(unique(cl_model$admission_source_id))
+length(unique(cl_model$discharge_disposition_id))
+
+##NULL value
+length(which((cl_model[,5] == "18") |(cl_model[,5] == "26") ))
+[1] 3691
+
+
