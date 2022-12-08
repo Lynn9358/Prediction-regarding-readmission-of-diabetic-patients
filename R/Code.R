a409da8136b57871e259ebf7c4182027f4adb2a5
Wenjing Li <wjingli@umich.edu>
2022-12-08 12:00:34 -0500

    clear data

--- R/Code.R
+++ R/Code.R
@@ -1,11 +1,12 @@
 full_model = read.csv("~/Downloads/FINAL/diabetic_data.csv")
+cl_model = subset(full_model,select = -c(encounter_id,patient_nbr,weight,payer_code, medical_specialty,examide,citoglipton))
 
 a = vector()
 b = list()
 
-for(i in 1:50){
-a[i] = length(which(full_model[,i] == "?"))
-b[[i]] = unique(full_model[,i])
+for(i in 1:ncol(cl_model)){
+a[i] = length(which(cl_model[,i] == "?"))
+b[[i]] = unique(cl_model[,i])
 }
 
 print(a)
